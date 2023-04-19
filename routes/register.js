02ad53e990f58f4122fd34bfcc2131ed000a0f5e
Agustin Maillet <agustinmaillet47@gmail.com>
2023-04-19 19:02:53 -0300

    A blank app

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,9 @@
-// This is the route for the register page domain.xyz/register
+// This is the route for the register page domain.xyz/registrarse
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    
+    res.render('usuarios/registrarse/index');
 });
 
 router.post('/', (req, res) => {
